c9a53a44a584ae7b60813cecaee19bf99e6f47bd
Victoria <victoria.morones@gmail.com>
2022-06-15 21:13:57 -0500

    comentario en app.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {StatusBar, Text, View} from 'react-native';
 import Navigation from './src/Navigation/Navigation';
-import { AuthProvider } from './src/context/AuthContext';
+import { AuthProvider } from './src/context/AuthContext'; // comentario prueba para merge
 import 'react-native-gesture-handler'
 
 //import {AuthProvider} from './src/context/AuthContext';
